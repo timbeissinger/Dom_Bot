25d213a5b5fa9477def61bec1259d08b418454e6
Tim Beissinger <beissinger@ucdavis.edu>
2014-07-16 09:34:35 -0700

    Plotted SFS Correctly (with inbreeding)

---
+++ scripts/compare_inb_noInb.R
@@ -0,0 +1,14 @@
+################################################################################
+### This script compares SFS estimated with Inb = 1 to that estimated with   ###
+### Inb = 0                                                                  ###
+################################################################################
+
+### Timothy M. Beissinger
+### 7-16-2014
+
+### Load each sfs
+sfsInb <- exp(scan("../SFS/BKN.sfs"))
+sfsNoInb <- exp(scan("../SFS/BKN_no_inb.sfs"))
+
+### Compare
+sfsInb - sfsNoInb # identical...
