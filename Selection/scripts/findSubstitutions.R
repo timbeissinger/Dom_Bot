c6b3cd1d48a370bcfcbfdc82fa17ec546377cbcf
Tim Beissinger <beissinger@ucdavis.edu>
2014-08-18 11:32:32 -0700

    findSubstitutions.R updated

--- Selection/scripts/findSubstitutions.R
+++ Selection/scripts/findSubstitutions.R
@@ -34,23 +34,53 @@ whichAreSubs <- function(data,outCols=5,popCols=6:41,missing.limit=0.5){
     if(length(out)>0 & length(pop)>0 & length(intersect(out,pop))==0) return ("Sub")
 }
 
+### Index by millions
+length <- nrow(SNPs) # count number of SNPs
+floor <- floor(length/1000000) # find lowest million
+starts <-c(1, c(1:floor)*1000000 +1)
+stops <- c(starts[1:{length(starts)-1}]+999999,length)
+
 # Trip vs. Maize & Teo
-TvMT_result<- apply(SNPs,1,whichAreSubs,outCols=5,popCols=6:41,missing.limit=0.2) # trip vs mays
+TvMT_result <- c()
+for(c in 1:length(starts)){
+    print(paste(starts[c],":",stops[c],sep=""))
+    SNPsub <- SNPs[starts[c]:stops[c],]
+    TvMT_result[starts[c]:stops[c]] <- apply(SNPsub,1,whichAreSubs,outCols=5,popCols=6:41,missing.limit=0.2) # trip vs mays
+}
+#TvMT_result<- apply(SNPs,1,whichAreSubs,outCols=5,popCols=6:41,missing.limit=0.2) # trip vs mays
 TvMT <- SNPs[which(as.character(TvMT_result)=="Sub"),c(1:4,5,6)]
 write.table(TvMT,file="TvMT.txt",row.names=F,col.names=T,quote=F,sep="\t")
 
 # Trip vs. Maize
-TvM_result<- apply(SNPs,1,whichAreSubs,outCols=5,popCols=19:41,missing.limit=0.2) # trip vs mays
+TvM_result <- c()
+for(c in 1:length(starts)){
+    print(paste(starts[c],":",stops[c],sep=""))
+    SNPsub <- SNPs[starts[c]:stops[c],]
+    TvM_result[starts[c]:stops[c]] <- apply(SNPsub,1,whichAreSubs,outCols=5,popCols=19:41,missing.limit=0.2) # trip vs maize
+}
+#TvM_result<- apply(SNPs,1,whichAreSubs,outCols=5,popCols=19:41,missing.limit=0.2) # trip vs maize
 TvM <- SNPs[which(as.character(TvM_result)=="Sub"),c(1:4,5,19)]
 write.table(TvM,file="TvM.txt",row.names=F,col.names=T,quote=F,sep="\t")
 
 # Trip vs. Teo
-TvT_result<- apply(SNPs,1,whichAreSubs,outCols=5,popCols=6:18,missing.limit=0.2) # trip vs mays
+TvT_result <- c()
+for(c in 1:length(starts)){
+    print(paste(starts[c],":",stops[c],sep=""))
+    SNPsub <- SNPs[starts[c]:stops[c],]
+    TvT_result[starts[c]:stops[c]] <- apply(SNPsub,1,whichAreSubs,outCols=5,popCols=6:18,missing.limit=0.2) # trip vs teo
+}
+#TvT_result<- apply(SNPs,1,whichAreSubs,outCols=5,popCols=6:18,missing.limit=0.2) # trip vs teo
 TvT <- SNPs[which(as.character(TvT_result)=="Sub"),c(1:4,5,6)]
 write.table(TvT,file="TvT.txt",row.names=F,col.names=T,quote=F,sep="\t")
 
 # Teo vs. Maize
-TevM_result <- apply(SNPs,1,whichAreSubs,outCols=6:18,popCols=19:41,missing.limit=0.2) # teo vs maize
+TevM_result <- c()
+for(c in 1:length(starts)){
+    print(paste(starts[c],":",stops[c],sep=""))
+    SNPsub <- SNPs[starts[c]:stops[c],]
+    TevM_result[starts[c]:stops[c]] <- apply(SNPsub,1,whichAreSubs,outCols=6:18,popCols=19:41,missing.limit=0.2) # teo vs maize
+}
+#TevM_result <- apply(SNPs,1,whichAreSubs,outCols=6:18,popCols=19:41,missing.limit=0.2) # teo vs maize
 TevM <- SNPs[which(as.character(TevM_result)=="Sub"),c(1:4,6,19)]
 write.table(TevM,file="TevM.txt",row.names=F,col.names=T,quote=F,sep="\t")
 
